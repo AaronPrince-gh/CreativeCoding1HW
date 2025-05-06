c32ecf7ab3d4bae8fa7fd3c8c9566ff21a7ee08f
AaronPrince-gh <aaron.prince@umconnect.umt.edu>
2025-05-05 22:22:04 -0600

    finalfinal

--- Final/final.js
+++ Final/final.js
@@ -45,7 +45,7 @@ function touchStarted() {
   }
 
 function preload() {
-    song = loadSound("worm.mp3");
+    //song = loadSound("worm.mp3");
     songplaying = false;
 }
 
@@ -53,6 +53,7 @@ function setup() {
     fill(0);
     createCanvas(2000, 1000);
     rect(0, 0, 2000, 1000);
+    updateState();
 }
 
 //function mousePressed()// {
@@ -63,6 +64,9 @@ function setup() {
    // }
 //}
 
+var timer = 0;
+var state = 0;
+
 function draw() {
     //bg
     background(bgcolor[0], bgcolor[1], bgcolor[2])
@@ -72,9 +76,13 @@ function draw() {
     //music
 
     //update
+    drawText();
     drawChar();
     charMovement();
     updateProj();
+    textSize(35);
+    textFont("Courier New");
+    text("Click to mute/unmute.", 200, 986);
     if (snowArray.length < 150) {
         append(snowArray, new projectile(Math.floor(Math.random() * 25), Math.floor(Math.random() * 25), Math.floor(Math.random() * 10), Math.floor((Math.random() * 10) + 1), Math.floor((Math.random() * 10) + 1), [255, 255, 255], 100))
     }
@@ -83,6 +91,138 @@ function draw() {
     }
     
 }
+
+function sleep(millisecondsDuration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecondsDuration);
+  })}
+
+
+function updateState() {
+    sleep(2000).then(function() {
+            state += 1;
+            updateState();
+    })
+}
+
+function drawText() {
+    if (state == 1) {
+        textFont("Helvetica");
+        textSize(20);
+    text("the rabbit", 950, 50);
+    }
+    if (state == 2) {
+        textFont("Helvetica");
+        textSize(20);
+    text("black fur", 950, 50);
+    }
+    if (state == 3) {
+        textFont("Helvetica");
+        textSize(20);
+    text("white snow", 950, 50);
+    }
+    if (state == 4) {
+        textFont("Helvetica");
+        textSize(20);
+    text("cold and hungry", 950, 50);
+    }
+    if (state == 5) {
+        textFont("Helvetica");
+        textSize(20);
+    text("cannot grow", 950, 50);
+    }
+    if (state == 6) {
+        textFont("Helvetica");
+        textSize(20);
+    text("trotting numbly", 950, 50);
+    }
+    if (state == 7) {
+        textFont("Helvetica");
+        textSize(20);
+    text("she'd had it,", 950, 50);
+    }
+    if (state == 8) {
+        textFont("Helvetica");
+        textSize(20);
+    text("golden sun", 950, 50);
+    }
+    if (state == 9) {
+        textFont("Helvetica");
+        textSize(20);
+    text("pale moon", 950, 50);
+    }
+    if (state == 10) {
+        textFont("Helvetica");
+        textSize(20);
+    text("now she lacked words", 950, 50);
+    }
+    if (state == 11) {
+        textFont("Helvetica");
+        textSize(20);
+    text("only dread", 950, 50);
+    }
+    if (state == 12) {
+        textFont("Helvetica");
+        textSize(20);
+    text("grew here", 950, 50);
+    }
+    if (state == 13) {
+        textFont("Helvetica");
+        textSize(20);
+    text("not the sun", 950, 50);
+    }
+    if (state == 14) {
+        textFont("Helvetica");
+        textSize(20);
+    text("nor the moon", 950, 50);
+    }
+    if (state == 15) {
+        textFont("Helvetica");
+        textSize(20);
+    text("how it hurts", 950, 50);
+    speed -= .01;
+    }
+    if (state == 16) {
+        textFont("Helvetica");
+        textSize(20);
+    text("to be gone", 950, 50);
+    speed -= .01;
+    }
+    if (state == 17) {
+        textFont("Helvetica");
+        textSize(20);
+    text("before you die", 950, 50);
+    speed -= .01;
+    }
+    if (state >= 18) {
+        textFont("Helvetica");
+        textSize(20);
+        text("", 950, 50);
+        Wolf();
+    }
+    if (state >= 20 && (abs(wolfTarget[0]) - charX <= 8) && (abs(wolfTarget[1]) - charY <= 8)) {
+            window.open('','_parent','');
+            window.close();
+    }
+}
+
+var wolfTarget = [];
+
+function Wolf() {
+    background(0);
+    clear();
+
+    wolfChase(charX, charY);
+    speed = 2;
+}
+function wolfChase(x, y) {
+    sleep(1200).then(function() {
+        fill(100);
+        circle(x, y, 50);
+        wolfTarget = [x, y];
+})
+}
+
 
 function updateProj() {
     for (i = 0; i < snowArray.length; i++) {
@@ -124,24 +264,26 @@ function drawChar() {
         circle(charX,charY,25);
 }
 
+var speed = 8;
+
 function charMovement()
 {
 
     if(keyIsDown(w))
     {
-        charYVel -= 8;   
+        charYVel -= speed;   
     }
     if(keyIsDown(s))
     {
-        charYVel += 8;   
+        charYVel += speed;   
     }
     if(keyIsDown(a))
     {
-        charXVel -= 8;   
+        charXVel -= speed;   
     }
     if(keyIsDown(d))
     {
-        charXVel += 8;   
+        charXVel += speed;   
     }
     if (abs(charXVel) + abs(charYVel) > 8){
         charX += charXVel*.71;
@@ -153,4 +295,16 @@ function charMovement()
     }
     charXVel = 0;
     charYVel = 0;
+    if (charX > 2000) {
+        charX = 2000;
+    }
+    if (charX < 0) {
+        charX = 0;
+    }
+    if (charY > 1000) {
+        charY = 1000;
+    }
+    if (charY < 0) {
+        charY = 0;
+    }
 }
